b7c6477ab7e8b8ee97806534ab8f8aa5db7758a4
Kamisora <rakamiso575@gmail.com>
2025-03-31 21:29:25 +0800

    添加个人页面

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,14 +1,28 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/components/EditScreenInfo';
+import { StyleSheet, Image, View as RNView, TouchableOpacity } from 'react-native';
 import { Text, View } from '@/components/Themed';
+import { Ionicons } from '@expo/vector-icons';
+import { useState } from 'react';
 
 export default function TabTwoScreen() {
+  const [loginStatus, setLoginStatus] = useState(false);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
+      <View style={styles.person}>
+        {loginStatus ? (
+          <RNView style={styles.profileContainer}>
+            <Text style={styles.username}>AtlasKK</Text>
+          </RNView>
+        ) : (
+          <RNView style={styles.notLoggedInContainer}>
+            <TouchableOpacity style={styles.loginButton}>
+              <Text style={styles.loginButtonText}>登录</Text>
+            </TouchableOpacity>
+          </RNView>
+        )}
+      </View>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      </View>
     </View>
   );
 }
@@ -19,13 +33,49 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  title: {
-    fontSize: 20,
+  person: {
+    backgroundColor: 'white',
+    marginTop: 70,
+    width: 340,
+    borderRadius: 20,
+    height: 100,
+    padding: 20,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 2,
+  },
+  profileContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: 'transparent',
+  },
+  username: {
+    fontSize: 18,
     fontWeight: 'bold',
+    marginLeft: 15,
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  notLoggedInContainer: {
+    alignItems: 'center',
+    paddingVertical: 10,
+    backgroundColor: 'transparent',
   },
+  notLoggedInText: {
+    fontSize: 16,
+    color: '#888888',
+    marginTop: 10,
+    marginBottom: 15,
+  },
+  loginButton: {
+    backgroundColor: '#69abf1',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 20,
+  },
+  loginButtonText: {
+    color: 'white',
+    fontSize: 14,
+    fontWeight: '600',
+  }
 });
